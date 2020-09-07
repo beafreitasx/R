3561d824aa159f6aec3c4310e8c637633ba6e3a8
Beatriz Freitas <beatryzfreitas@hotmail.com>
2020-09-07 17:32:10 -0300

    Update criando-graficos.R

--- R-para-Analise-de-Dados/Módulo-1/criando-graficos.R
+++ R-para-Analise-de-Dados/Módulo-1/criando-graficos.R
@@ -8,11 +8,9 @@ head(carros)
 hist(carros$mpg)
 
 ##Gráfico de dispersão
-
 plot(carros$mpg, carros$cyl) #função requer dois parâmetros
 
 ##Agora usando pacote(ggplot2)
-
 library(ggplot2)
 ggplot(carros,aes(am))+ #para usar a função ggplot()é necessário dois parâmetros: o dataset e a coluna. A cada vetor existente na coluna,
   #será criada uma barra
