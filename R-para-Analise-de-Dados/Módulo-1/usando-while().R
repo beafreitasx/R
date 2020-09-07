85aec5ec55462dbca7778f6a0f50342b47d02608
Beatriz Freitas <beatryzfreitas@hotmail.com>
2020-09-07 16:57:04 -0300

    adicionando atv 8

---
+++ R-para-Analise-de-Dados/Módulo-1/usando-while().R
@@ -0,0 +1,12 @@
+# Estrutura de Repetição: aprendendo funções para analisar dados
+
+##While() é utilizada para repetir uma operação por um determinado número de vezes.
+
+### while(condição){
+###       código}
+
+i <- 0
+while (i <= 10){
+  print(i)
+  i = i+1
+}
