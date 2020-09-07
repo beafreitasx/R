c2a5065e9f505e657a9f801d9bf55b01462c60d4
Beatriz Freitas <beatryzfreitas@hotmail.com>
2020-09-07 16:54:58 -0300

    criando atv 7

---
+++ R-para-Analise-de-Dados/Módulo-1/usando-for().R
@@ -0,0 +1,9 @@
+#Estrutura de Repetição: aprendendo funções para analisar dados
+
+## A função For() é utilizada para acessar cada elemento de uma sequência (vetor, coluna de matriz ou data frame matriz).
+
+#for(valor in sequencia){
+#    código}
+
+for (i in seq(12)){
+  print(i)}
